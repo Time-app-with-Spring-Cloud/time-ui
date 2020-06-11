553962fb8c53aba1851f02afb32d4c0b9f862249
Pedro Galan <pedro.galan@piksel.com>
2020-06-11 08:00:00 +0100

    Spring boot app skeleton

---
+++ src/main/java/es/pedrogalan/time/ui/TimeUiApplication.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/pedrogalan/time/ui/TimeUiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
